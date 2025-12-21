a24f0477911f2c616f53df39c9cc6df71dc37d69
Mexson Fernandes <robomex2020@gmail.com>
2025-12-21 10:21:46 +0530

    :hammer: embed youtube

--- pages/index.vue
+++ pages/index.vue
@@ -142,16 +142,21 @@ export default {
       ],
     }
   },
-  head() {
-    return {
-      script: [
-        { src: 'https://apis.google.com/js/platform.js' }
-      ]
-    }
-  },
+
   mounted() {
     (function (C, A, L) { const p = function (a, ar) { a.q.push(ar); }; const d = C.document; C.Cal = C.Cal || function () { const cal = C.Cal; const ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar); return; } p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
       Cal("init", {origin:"https://app.cal.com"});
+
+    // Load YouTube Subscribe script
+    if (window.gapi && window.gapi.ytsubscribe) {
+      window.gapi.ytsubscribe.go();
+    } else {
+      const script = document.createElement('script');
+      script.src = 'https://apis.google.com/js/platform.js';
+      script.async = true;
+      script.defer = true;
+      document.head.appendChild(script);
+    }
   }
 }
 </script>
